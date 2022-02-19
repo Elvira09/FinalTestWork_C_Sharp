a6600b22fc2a9e0213e352030ab5b1129019fb10
Elvira09 <zag0909@gmail.com>
2022-02-19 18:39:00 +0300

    created a method PrintArray

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void PrintArray(string[] array)
+{
+	Console.Write("[");
+	for (int i = 0; i < array.Length; i++)
+	{
+		Console.Write($"\" {array[i]}\"");
+		if (i != array.Length - 1)
+		{
+			Console.Write(", ");
+		}
+	}
+	Console.Write("]");
+}
+
